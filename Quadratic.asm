51fb067b712d4661a7f0abd0e5d9e4feeaa45a4e
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-03-30 23:18:10 +0530

    Create Quadratic.asm

---
+++ Quadratic.asm
@@ -0,0 +1,15 @@
+		PRESERVE8          
+                THUMB      
+                AREA    |.text|, CODE, READONLY
+		EXPORT __main			
+__main 
+	MOVS r0,#5 ;x value
+	MOVS r4,r0
+	MOVS r1,#3
+	MOVS r2,#2
+	MULS r0,r0,r0
+	MULS r0,r2,r0
+	ADDS r0,r4,r0
+	ADDS r0,r0,r1
+STOP B STOP
+	END
