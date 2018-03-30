bd401786bf52cf88e637449a825c3cfdc0bcf4cb
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-03-30 23:20:00 +0530

    Create Copy10Bytes.asm

---
+++ Copy10Bytes.asm
@@ -0,0 +1,20 @@
+	PRESERVE8 ; Indicate the code here preserve  
+; 8 byte stack alignment         
+                     THUMB     ; Indicate THUMB code is used       
+                 AREA    |.text|, CODE, READONLY
+			   
+              EXPORT __main			 
+; Start of CODE area 
+__main
+		ldr r0,=0x20000000
+		ldr r1,=0x20000020
+		movs r2,#10
+loop    
+		ldrb r3,[r0]
+		strb r3,[r1]
+		adds r0,r0,#1
+		adds r1,r1,#1
+		subs r2,r2,#1
+		bne loop
+stop    b stop
+        END
