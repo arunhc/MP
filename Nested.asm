8f49616c7033e229769b01869f01a4c6810eb3e8
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-03-30 23:13:35 +0530

    Update Nested.asm

--- Nested.asm
+++ Nested.asm
@@ -10,3 +10,29 @@ func
 	BL func2
 	POP {PC}
 	END
+++++++++++++++++++++++++++
+FUNCTION 2
+	PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+
+	EXPORT func2
+
+func2
+	MOVS R1, #10
+	BX LR
+	END
+	
+++++++++++++++++++++++++++++++
+FUNCTION CALLER
+	PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+
+	EXPORT __main
+	EXTERN func
+__main
+	BL func
+	MOV R0,#40
+	NOP
+	END
