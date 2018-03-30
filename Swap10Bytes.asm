8216faef875dfbe2fec0112446507289950f8f01
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-03-30 23:16:17 +0530

    Create Swap10Bytes.asm

---
+++ Swap10Bytes.asm
@@ -0,0 +1,19 @@
+		PRESERVE8          
+                THUMB      
+                AREA    |.text|, CODE, READONLY
+		EXPORT __main			
+__main 	
+	LDR r0,=0x20000200
+	LDR r1,=0x20000100
+	MOVS r2,#10
+up
+	LDRB r3,[r0]
+	LDRB r4,[r1]
+	STRB r4,[r0]
+	STRB r3,[r1]
+	ADDS r0,r0,#1
+	ADDS r1,r1,#1
+	SUBS r2,r2,#1
+	BNE up
+STOP B STOP
+	END
