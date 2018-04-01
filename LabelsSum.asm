828738801f89ab5d38f721eb7dd2bfd67c6b71f6
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-02 01:20:10 +0530

    Update LabelsSum.asm

--- LabelsSum.asm
+++ LabelsSum.asm
@@ -7,12 +7,12 @@ PRESERVE8 ; Indicate the code here preserve
 __main 	
 			LDR	R0,=0X00000001
 			LDR	R1,=0X00000002
-strt3			cmp r0,#10
+start3			cmp r0,#10
 			ble start2
 			movs r0,#0
 			b stop
 start2		adds r0,r0,#1			
-			b strt3
+			b start3
 			
 stop	      B	   stop		 		;stop program
 	          END
