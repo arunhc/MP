cade91f35a26b14d1bc1b939c3c58b55d77ca17a
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-01 20:31:42 +0530

    Update 2x+9.asm

--- 2x+9.asm
+++ 2x+9.asm
@@ -1,16 +1,16 @@
-PRESERVE8
+	PRESERVE8
 	THUMB
 	AREA |.text|, CODE, READONLY
 
 	EXPORT __main
 		
 func1
-   MOVS r0,#2
-   MOVS R1, #2
-   MULS r0,r1,r0
-   MOVS R2,#9
-   ADDS R0,R0,R2
-   BX LR
+   	MOVS r0,#2
+   	MOVS R1, #2
+   	MULS r0,r1,r0
+   	MOVS R2,#9
+   	ADDS R0,R0,R2
+   	BX LR
 
 __main
 	BL func1
