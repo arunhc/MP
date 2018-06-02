8950f80702dfe3dad287ffd3e3a6263dee1eb69d
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-02 16:05:52 +0530

    Update 4b.c

--- 4b.c
+++ 4b.c
@@ -5,25 +5,19 @@
 #include "Driver\DrvSYS.h"
 
 void Init_LED()
-{
-
-	DrvGPIO_Open(E_GPA, 12, E_IO_OUTPUT);
-	DrvGPIO_Open(E_GPA, 13, E_IO_OUTPUT);
-	DrvGPIO_Open(E_GPA, 14, E_IO_OUTPUT);
-	DrvGPIO_SetBit(E_GPA, 12);
-	DrvGPIO_SetBit(E_GPA, 13);
-	DrvGPIO_SetBit(E_GPA, 14);
+{DrvGPIO_Open(E_GPA, 12, E_IO_OUTPUT);
+DrvGPIO_Open(E_GPA, 13, E_IO_OUTPUT);
+DrvGPIO_Open(E_GPA, 14, E_IO_OUTPUT);
+DrvGPIO_SetBit(E_GPA, 12);
+DrvGPIO_SetBit(E_GPA, 13);
+DrvGPIO_SetBit(E_GPA, 14);
 }    
-
 int main (void)
-{
-	UNLOCKREG();
-    DrvSYS_Open(48000000);
-	LOCKREG();
-
-	Init_LED();
-						   
-	while (1)
+{UNLOCKREG();
+DrvSYS_Open(48000000);
+LOCKREG();
+Init_LED();
+ while(1)
 	{		 
     DrvGPIO_ClrBit(E_GPA,12);
 	DrvSYS_Delay(1000000); 		   
