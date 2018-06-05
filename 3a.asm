238b7b88491cd658923f290698865fd86444beb1
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-05 21:18:19 +0530

    Update 3a.asm

--- 3a.asm
+++ 3a.asm
@@ -1,19 +1,18 @@
-		PRESERVE8          
-                THUMB      
-                AREA    |.text|, CODE, READONLY
-		EXPORT __main			
-__main 	
-	LDR r0,=0x20000200
-	LDR r1,=0x20000100
-	MOVS r2,#10
-loop
-	LDRB r3,[r0]
-	LDRB r4,[r1]
-	STRB r4,[r0]
-	STRB r3,[r1]
-	ADDS r0,r0,#1
-	ADDS r1,r1,#1
-	SUBS r2,r2,#1
-	BNE loop
-	NOP
+	PRESERVE8
+	THUMB
+	AREA |.text|,CODE,READONLY
+	EXPORT __main
+__main
+	LDR R0,=0x20000100
+	LDR R1,=0x20000200
+	MOV R2,#0
+swap
+	LDRB R3,[R0,R2]
+	LDRB R4.[R1,R2]
+	STRB R3,[R1,R2]
+	STRB R4,[R0,R2]
+	ADD R2,R2,#1
+	CMP R2,#10
+	BLT swap
+stop B stop
 	END
