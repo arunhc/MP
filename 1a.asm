5c81409571d42a16a76173d3a4b96d08b6b334c6
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-05 21:14:41 +0530

    Update 1a.asm

--- 1a.asm
+++ 1a.asm
@@ -1,14 +1,11 @@
-	PRESERVE8       
-        THUMB   
-        AREA |.text|, CODE, READONLY
-			
-	EXPORT __main			 
-__main                 
-	LDR R3,=0x20000100
-	LDR R0,=0x20000050
-	LDMIA R3!,{R1,R2}
-	MOV SP,R0
-	PUSH {R1,R2}
-	POP {R4,R5}
-stop    B   stop			   
-               END    
+	PRESERVE8
+	THUMB
+	AREA |.text|,CODE,READONLY
+	EXPORT __main
+__main
+	MOV R0,#5
+	PUSH {R0}
+	POP {R1}
+stop	B	stop
+	END
+	
